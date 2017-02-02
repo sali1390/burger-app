158a93e7727e74d50a724c4c4c5ceb02be0d3b2a
Saleema Ali <sali1390@gmail.com>
2017-02-02 10:54:31 -0500

    Missing closing brackets

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,12 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "burgers_db"
+    });
+}
 
 connection.connect(function(err) {
   if (err) {
