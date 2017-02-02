bb2229b13e3f8a130356bbe7af8a144f74c5e1a4
Saleema Ali <sali1390@gmail.com>
2017-02-02 10:48:42 -0500

    Updated port info

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var controller = require("./controllers/burgers_controller.js")
 
