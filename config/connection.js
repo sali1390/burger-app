2e26dff4c0d02be570d5a9c280f9bf4aca1d5a0b
Saleema Ali <sali1390@gmail.com>
2017-02-02 09:56:52 -0500

    Adding files for Heroku and setting up DB add-on

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
